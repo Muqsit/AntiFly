03ec82093cd6eda8002b07e0e234ca57501c9d54
TutoGamerWalid <wiligangster@users.noreply.github.com>
2015-11-04 03:54:58 +0100

    Update AntiFly.php

--- src/MCrafters/AntiFly.php
+++ src/MCrafters/AntiFly.php
@@ -1,1 +1,20 @@
-IN DEV
+<?php
+
+namespace Mcrafters;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerKickEvent;
+use pocketmine\utils\Listener;
+use pocketmine\Player;
+use Pocketmine\Server;
+
+class Main extends PluginBase Implements Listener {
+
+   public function onEnable() {
+     $this->getLogger()->info("Enabled");
+   }
