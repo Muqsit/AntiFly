e342927ae29df1bc5d4aa3074f49a7e23b8e3469
Muqsit Rayyan <hackhack05@gmail.com>
2016-06-14 07:38:31 -0800

    Update AntiFly.php

--- src/MCrafters/AntiFly.php
+++ src/MCrafters/AntiFly.php
@@ -8,8 +8,12 @@ use pocketmine\utils\Config;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerKickEvent;
 use pocketmine\utils\Listener;
+use pocketmine\level\Level;
+use pocketmine\level\Positiom;
+use pocketmine\math\Vector3;
 use pocketmine\Player;
 use Pocketmine\Server;
+use pocketmine\block\Block;
 
 class AntiFly extends PluginBase Implements Listener {
 
@@ -20,3 +24,9 @@ class AntiFly extends PluginBase Implements Listener {
 
    public function PlayerMove(PlayerMoveEvent $event){
        $player = $event->getPlayer();
+       $beneath = $event->getPlayer()->getLevel()->getBlock($event->getPlayer()->floor()->subtract(0, 1));
+        if($beneath->getId() === 0)) {
+        $player->sendMessage("Are you flying?");
+        }
+    }
+}
